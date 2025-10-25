df5cca1b6306e5efe5504d64c460efdacba625e4
rain474 <lordraindance5@gmail.com>
2025-10-24 20:54:15 -0700

    Trainer transfer

--- include/constants/battle_partner.h
+++ include/constants/battle_partner.h
@@ -2,8 +2,9 @@
 #ifndef GUARD_CONSTANTS_BATTLE_PARTNERS_H
 #define GUARD_CONSTANTS_BATTLE_PARTNERS_H
 
-#define PARTNER_NONE 0
-#define PARTNER_STEVEN 1
-#define PARTNER_COUNT 2
+#define PARTNER_NONE                0
+#define PARTNER_STEVEN              1
+#define PARTNER_MEADOWS_RIVAL       2
 
+#define PARTNER_COUNT               3
 #endif  // GUARD_CONSTANTS_BATTLE_PARTNERS_H
