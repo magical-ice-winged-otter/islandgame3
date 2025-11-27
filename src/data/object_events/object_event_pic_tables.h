8e7eb940accda7dfc61c744f631bfd6f1ca6a4cb
poetahto <epic.potat@gmail.com>
2025-11-26 21:59:11 -0800

    Fix broken brendan+may cameo sprites

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -733,15 +733,7 @@ static const struct SpriteFrameImage sPicTable_Brendan2[] = {
 };
 
 static const struct SpriteFrameImage sPicTable_May2[] = {
-    overworld_frame(gObjectEventPic_May2, 2, 4, 0),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 1),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 2),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 3),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 4),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 5),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 6),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 7),
-    overworld_frame(gObjectEventPic_May2, 2, 4, 8),
+    overworld_ascending_frames(gObjectEventPic_May2, 2, 4),
 };
 
 
