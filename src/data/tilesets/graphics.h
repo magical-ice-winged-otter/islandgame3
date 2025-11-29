963c07762efcaab97ebca8a3a02e8d53bc30300f
poetahto <epic.potat@gmail.com>
2025-11-29 11:38:10 -0800

    Create new primary for aetheria, update pleasant + bellhaven for new warp transition

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -2398,3 +2398,22 @@ const u16 gTilesetPalettes_islandgame_kanto_pokemontower[][16] =
 };
 
 const u32 gTilesetTiles_islandgame_kanto_pokemontower[] = INCBIN_U32("data/tilesets/secondary/islandgame_kanto_pokemontower/tiles.4bpp.fastSmol");
+
+const u16 gTilesetPalettes_islandgame_lumine_aetheria[][16] =
+{
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/primary/islandgame_lumine_aetheria/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_islandgame_lumine_aetheria[] = INCBIN_U32("data/tilesets/primary/islandgame_lumine_aetheria/tiles.4bpp.lz");
