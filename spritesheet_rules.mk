244568c0eb5baff17578a2d6216c1f022f92fdac
ressykins <resistantpwnage@gmail.com>
2026-07-05 12:58:34 -0400

    cynthia summer sprite

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -279,7 +279,7 @@ $(OBJEVENTGFXDIR)/people/idol.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/people/beauty_dppt.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
-$(OBJEVENTGFXDIR)/people/cynthia.4bpp: %.4bpp: %.png
+$(OBJEVENTGFXDIR)/people/cynthia_alt.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 4
 
 $(OBJEVENTGFXDIR)/people/painter.4bpp: %.4bpp: %.png
